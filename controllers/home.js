a96994473bb9de87362802c193b9f360eacd6583
Carlos Henckes <henckes@uol.com.br>
2018-05-08 15:18:25 -0300

    improvement
    improvement

--- controllers/home.js
+++ controllers/home.js
@@ -1,27 +1,14 @@
-const https = require("https");
+
 const url = 'https://arotas.herokuapp.com/loc/percorridas';
 
 module.exports = function (app) {
+    let list;
     var HomeController = {
         index: function (req, res) {
             res.render('home/index');
         },
-        corridas: function (request, response) {
-            var lista = "";
-            https.get(url, res => {
-                res.setEncoding("utf8");
-                res.on("data", data => {
-                    //lista += data;
-                });
-                res.on("end", () => {
-                    lista = JSON.parse(lista);
-                    console.log(lista);
-                });
-                res.on("error", function (err) {
-                    console.log(err);
-                });
-            });
-            response.render('home/corridas', { lista : '{"placa":"CSA6410"}' });
+        corridas: function (req, res) {            
+            res.render('home/corridas');
         }
     };
     return HomeController;
