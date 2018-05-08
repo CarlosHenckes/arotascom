47cb4efe356cd384f93e9f2a821d3e9f96ed3471
Carlos Henckes <henckes@uol.com.br>
2018-05-07 22:32:53 -0300

    lista
    added corridas list

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser'); 
 var porta = process.env.PORT || 3000;
 app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 load('models')
--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,27 @@
+const https = require("https");
+const url = 'https://arotas.herokuapp.com/loc/percorridas';
+
 module.exports = function (app) {
     var HomeController = {
         index: function (req, res) {
             res.render('home/index');
+        },
+        corridas: function (request, response) {
+            var lista = "";
+            https.get(url, res => {
+                res.setEncoding("utf8");
+                res.on("data", data => {
+                    //lista += data;
+                });
+                res.on("end", () => {
+                    lista = JSON.parse(lista);
+                    console.log(lista);
+                });
+                res.on("error", function (err) {
+                    console.log(err);
+                });
+            });
+            response.render('home/corridas', { lista : '{"placa":"CSA6410"}' });
         }
     };
     return HomeController;
