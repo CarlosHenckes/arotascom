db0a81cfea9c2c6b9a049ba132f1ba93c2c5c0cd
Carlos Henckes <henckes@uol.com.br>
2018-05-10 10:42:53 -0300

    final version 1.0
    final version 1.0

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,4 @@
-
+var Request = require("request");
 const url = 'https://arotas.herokuapp.com/loc/percorridas';
 
 module.exports = function (app) {
@@ -7,16 +7,33 @@ module.exports = function (app) {
         index: function (req, res) {
             res.render('home/index');
         },
-        corridas: function (req, res) {            
+        corridas: function (req, res) {
             res.render('home/corridas');
         },
-        frota: function (req, res){
+        frota: function (req, res) {
             res.render('home/frota');
         },
-        cadastro: function(req, res){
+        cadastro: function (req, res) {
             res.render('home/cadastro');
         },
-        cadastrar: function(req, res){
+        cadastrar: function (req, res) {
+            Request.post({
+                "headers": { "content-type": "application/json" },
+                "url": "https://arotas.herokuapp.com/loc",
+                "body": JSON.stringify({
+                    "placa": req.body.placa,
+                    "nomeMotorista": req.body.nomeMotorista,
+                    "localizacao": {
+                        "latitude": -23.574076,
+                        "longitude": -46.623360
+                    },
+                    "status": "LIVRE"
+                })
+            }, (error, response, body) => {
+                if (error) {
+                    return console.dir(error);
+                }
+            });
             res.redirect('/frota');
         }
     };
