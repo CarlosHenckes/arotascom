7dc9bebc0e6eee99dd91227b6b2ad5ec6ef0d30d
Carlos Henckes <henckes@uol.com.br>
2018-05-10 16:29:28 -0300

    correct re run link

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var load = require('express-load');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 var porta = process.env.PORT || 3000;
 app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
@@ -15,6 +16,7 @@ load('models')
   .then('routes')
   .into(app);
 
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.listen(porta, function () {
   console.log("Aplicação no ar.");
